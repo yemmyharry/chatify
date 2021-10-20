d6d903957bc6e738b21d65f7e31062fadc144880
yemmyharry <yemmyharry@gmail.com>
2021-10-20 20:42:45 +0100

    set up routes for chat

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yemmyharry/chatty/pkg/websocket"
+	"net/http"
+
+
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Distributed Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
